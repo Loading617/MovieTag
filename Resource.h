6ccb38ff1dd4e28d4f8ba9791f42e95dc80c2b21
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-05 05:31:41 -0400

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -5,13 +5,13 @@
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_VITAGEDITOR_DIALOG	102
+#define IDD_MOVIETAG_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_VITAGEDITOR			107
+#define IDI_MOVIETAG			107
 #define IDI_SMALL				108
-#define IDC_VITAGEDITOR			109
+#define IDC_MOVIETAG			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
