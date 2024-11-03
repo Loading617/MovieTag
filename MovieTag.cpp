43346c64211d482d2d367b678e8a133fcfb610c4
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-03 16:43:41 -0500

    Update MovieTag.cpp

--- MovieTag.cpp
+++ MovieTag.cpp
@@ -1,1 +1,18 @@
-#include 
+#include <wx/wx.h>
+#include <wx/wxapp.h>
+
+class MovieTagApp : public wxApp {
+public:
+    virtual bool OnInit() override {
+        // Create the main window
+        wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "AttackPS1", wxDefaultPosition, wxSize(800, 600));
+
+        // Set the frame icon
+        frame->SetIcon(wxIcon(".ico"));
+
+        // Show the frame
+        frame->Show(true);
+
+        return true;
+    }
+};
